6f84ec6de33dd1d2ce611c82ec9ca58998b9e727
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-12-09 14:16:59 -0200

    styles: Update color pallette

--- utils/colors.js
+++ utils/colors.js
@@ -1,9 +1,17 @@
-const $red = '#cf6766';
+const $black = '#191919';
+const $gray = '#c6c6c6';
+const $lightRed = '#cf6766';
+const $darkRed = '#330902';
+const $lightGreen = '#b4bd8e';
 const $darkBlue = '#031424';
 const $lightBlue = '#8eaebd';
 
 export {
-  $red,
+  $black,
+  $gray,
+  $lightRed,
+  $darkRed,
+  $lightGreen,
   $darkBlue,
   $lightBlue,
 };
