17e83d9bcd5ff926be3797c0d0b1d71e696308b0
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-12-10 21:16:06 -0200

    styles: Change colors values

--- utils/colors.js
+++ utils/colors.js
@@ -2,11 +2,11 @@ const $black = '#191919';
 const $white = '#f6f6f6';
 const $grey = '#d7d7d7';
 const $lightGrey = '#e5e5e5';
-const $lightRed = '#ba5c5b';
+const $lightRed = '#ff2800';
 const $darkRed = '#330902';
 const $lightGreen = '#b4bd8e';
-const $darkBlue = '#1c2b39';
-const $lightBlue = '#8eaebd';
+const $darkBlue = '#0e1a40';
+const $lightBlue = '#222f5b';
 
 export {
   $black,
