df985ab24cb386f7064d770f6155499bb96a5c0e
rafael.maia1 <rafael.maia1@ibm.com>
2018-12-11 15:54:36 -0200

    feature: improve animation for new set/folder

--- routes/MainNavigation.js
+++ routes/MainNavigation.js
@@ -1,32 +1,26 @@
 import React from 'react';
 import { createMaterialTopTabNavigator, createAppContainer } from 'react-navigation';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import CustomBottomTabBar from './CustomBottomTabBar';
+import BottomTabBar from './CustomNavigator/BottomTabBar';
 import Home from '../screens/Home';
 import Trash from '../screens/Trash';
 import { $black, $grey, $lightBlue } from '../utils/colors';
 
-const createTab = (screen, label, icon) => ({
-  screen,
-  navigationOptions: {
-    tabBarLabel: label,
-    tabBarIcon: ({ tintColor }) => ( // eslint-disable-line react/prop-types
-      <MaterialCommunityIcons name={icon} color={tintColor} size={25} />
-    ),
-  },
-});
-
 const NavigationTabs = createMaterialTopTabNavigator(
   {
-    Home: createTab(Home, 'Home', 'home'),
-    Trash: createTab(Trash, 'Trash', 'trash-can'),
+    Home: {
+      screen: Home,
+    },
+    Trash: {
+      screen: Trash,
+    },
   },
   {
     initialRouteName: 'Home',
     tabBarPosition: 'bottom',
-    swipeEnabled: true,
-    animationEnabled: true,
-    tabBarComponent: props => <CustomBottomTabBar {...props} />,
+    swipeEnabled: false,
+    animationEnabled: false,
+    lazy: true,
+    tabBarComponent: props => <BottomTabBar {...props} />,
     tabBarOptions: {
       activeTintColor: $lightBlue,
       inactiveTintColor: $black,
