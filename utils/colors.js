bb78790fa599043c6e6f7b33b8a31481699a89ea
rafael.maia1 <rafael.maia1@ibm.com>
2018-12-11 17:59:53 -0200

    styles: add new color and hexToRgb function

--- utils/colors.js
+++ utils/colors.js
@@ -1,3 +1,13 @@
+const hexToRgb = (hex) => {
+  const hexNumber = hex.replace('#', '');
+  const r = parseInt(hexNumber.substring(0, 2), 16);
+  const g = parseInt(hexNumber.substring(2, 4), 16);
+  const b = parseInt(hexNumber.substring(4, 6), 16);
+
+  const result = `rgb(${r},${g},${b})`;
+  return result;
+};
+
 const $black = '#191919';
 const $white = '#f6f6f6';
 const $grey = '#d7d7d7';
@@ -7,8 +17,10 @@ const $darkRed = '#330902';
 const $lightGreen = '#b4bd8e';
 const $darkBlue = '#0e1a40';
 const $lightBlue = '#222f5b';
+const $yellow = '#d3a625';
 
 export {
+  hexToRgb,
   $black,
   $white,
   $grey,
@@ -18,4 +30,5 @@ export {
   $lightGreen,
   $darkBlue,
   $lightBlue,
+  $yellow,
 };
