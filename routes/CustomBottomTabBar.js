b5e3e9c46c2858e6abf6d1a55a22624396f43713
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-12-10 22:53:41 -0200

    feat: Create animation to show new set option

--- routes/CustomBottomTabBar.js
+++ routes/CustomBottomTabBar.js
@@ -1,9 +1,10 @@
-import React, { PureComponent } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import {
-  View, TouchableOpacity, StyleSheet,
+  View, TouchableOpacity, StyleSheet, Dimensions, Animated,
 } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { $darkBlue } from '../utils/colors';
 
 class CustomBottomTabBar extends PureComponent {
   static propTypes = {
@@ -19,6 +20,9 @@ class CustomBottomTabBar extends PureComponent {
 
   state = {
     newSetButtonClicked: false,
+    hiddenIconsContainerSize: new Animated.Value(0),
+    hiddenIconSize: new Animated.Value(0),
+    hiddenIconOpacity: new Animated.Value(0),
   }
 
   getIcon = (condition, icon) => {
@@ -30,8 +34,52 @@ class CustomBottomTabBar extends PureComponent {
     return condition ? [icon, activeTintColor] : [`${icon}-outline`, inactiveTintColor];
   }
 
+  startNewSetAnimation = () => {
+    const {
+      hiddenIconSize, hiddenIconsContainerSize, hiddenIconOpacity, newSetButtonClicked,
+    } = this.state;
+
+    Animated.sequence([
+      Animated.timing(
+        hiddenIconsContainerSize,
+        {
+          toValue: newSetButtonClicked ? 1 : 0,
+          duration: 50,
+        },
+      ),
+      Animated.parallel([
+        Animated.timing(
+          hiddenIconSize,
+          {
+            toValue: newSetButtonClicked ? 65 : 0,
+            duration: 100,
+          },
+        ),
+        Animated.timing(
+          hiddenIconOpacity,
+          {
+            toValue: newSetButtonClicked ? 1 : 0,
+            duration: 100,
+          },
+        ),
+      ]),
+    ]).start();
+  }
+
+  toggleNewSet = () => {
+    this.setState(prev => ({ newSetButtonClicked: !prev.newSetButtonClicked }), () => {
+      this.startNewSetAnimation();
+    });
+  }
+
   render() {
-    const { newSetButtonClicked } = this.state;
+    const {
+      newSetButtonClicked,
+      hiddenIconSize,
+      hiddenIconOpacity,
+      hiddenIconsContainerSize,
+    } = this.state;
+
     const {
       navigation: { state: { index } },
       jumpTo,
@@ -47,18 +95,48 @@ class CustomBottomTabBar extends PureComponent {
     const [trashIcon, trashIconColor] = this.getIcon(isAtTrashScreen, 'trash-can');
     const [newSetIcon, newSetIconColor] = this.getIcon(newSetButtonClicked, 'plus-box');
 
+    const { height } = Dimensions.get('window');
+
+    const aditionalHiddenContainerStyles = {
+      transform: [{ scale: hiddenIconsContainerSize }],
+      height,
+    };
+
+    const aditionalHiddenIconStyles = {
+      bottom: hiddenIconSize,
+      transform: [{ scale: hiddenIconOpacity }],
+    };
+
+    const hiddenContainerStyles = {
+      ...styles.hiddenIconsContainer,
+      ...aditionalHiddenContainerStyles,
+    };
+
+    const hiddenIconStyles = { ...styles.hiddenIcons, ...aditionalHiddenIconStyles };
+
+    const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
+
     return (
-      <View style={styles.container}>
-        <TouchableOpacity onPress={jumpToHome} activeOpacity={0.75}>
-          <MaterialCommunityIcons name={homeIcon} size={25} color={homeIconColor} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}} activeOpacity={0.75}>
-          <MaterialCommunityIcons name={newSetIcon} size={25} color={newSetIconColor} />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={jumpToTrash} activeOpacity={0.75}>
-          <MaterialCommunityIcons name={trashIcon} size={25} color={trashIconColor} />
-        </TouchableOpacity>
-      </View>
+      <Fragment>
+        <View style={styles.container}>
+          <TouchableOpacity onPress={jumpToHome} activeOpacity={0.75}>
+            <MaterialCommunityIcons name={homeIcon} size={25} color={homeIconColor} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={this.toggleNewSet} activeOpacity={0.75}>
+            <MaterialCommunityIcons name={newSetIcon} size={25} color={newSetIconColor} />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={jumpToTrash} activeOpacity={0.75}>
+            <MaterialCommunityIcons name={trashIcon} size={25} color={trashIconColor} />
+          </TouchableOpacity>
+        </View>
+        <AnimatedTouchable style={hiddenContainerStyles} onPress={this.toggleNewSet}>
+          <Animated.View style={hiddenIconStyles}>
+            <TouchableOpacity onPress={jumpToTrash} activeOpacity={0.75}>
+              <MaterialCommunityIcons name="cards" size={30} color={$darkBlue} />
+            </TouchableOpacity>
+          </Animated.View>
+        </AnimatedTouchable>
+      </Fragment>
     );
   }
 }
@@ -69,6 +147,20 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     alignItems: 'center',
     height: 65,
+    width: '100%',
+  },
+  hiddenIconsContainer: {
+    position: 'absolute',
+    justifyContent: 'flex-end',
+    width: '100%',
+    top: 0,
+    left: 0,
+  },
+  hiddenIcons: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 150,
     width: '100%',
   },
 });
