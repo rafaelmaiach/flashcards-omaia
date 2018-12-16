b88e700cb8933f3982815ab926dba49607a66190
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-12-16 13:13:17 -0200

    feat: add change set title function

---
+++ components/NewSet/TitleModalEditor.js
@@ -0,0 +1,110 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Text, TouchableOpacity, View, StyleSheet, TextInput,
+} from 'react-native';
+import Modal from 'react-native-modal';
+import {
+  $white, $black, $lightBlue, $darkGreen,
+} from '../../utils/colors';
+
+class TitleModalEditor extends PureComponent {
+  static propTypes = {
+    navigation: PropTypes.object.isRequired,
+    toggleModalVisible: PropTypes.func.isRequired,
+    visible: PropTypes.bool.isRequired,
+  }
+
+  state = {
+    newTitle: '',
+  };
+
+  componentWillReceiveProps(nextProps) {
+    const { newTitle } = this.state;
+    const { navigation } = nextProps;
+    const title = navigation.getParam('title');
+
+    if (title !== newTitle) {
+      this.setState(() => ({ newTitle: title }));
+    }
+  }
+
+  onChangeText = newTitle => this.setState({ newTitle });
+
+  onCloseModal = () => {
+    const { newTitle } = this.state;
+    const { toggleModalVisible, navigation } = this.props;
+    navigation.setParams({ title: newTitle });
+
+    toggleModalVisible();
+  }
+
+  render() {
+    const { newTitle } = this.state;
+    const { visible } = this.props;
+
+    return (
+      <Modal
+        animationIn="fadeIn"
+        animationOut="fadeOut"
+        backdropColor={$black}
+        backdropOpacity={0.5}
+        isVisible={visible}
+        onBackButtonPress={this.onCloseModal}
+        onBackdropPress={this.onCloseModal}
+      >
+        <View style={styles.modalContent}>
+          <Text style={styles.editSetTitle}>EDIT TITLE</Text>
+          <TextInput
+            onChangeText={this.onChangeText}
+            style={styles.modalInput}
+            value={newTitle}
+          />
+          <View style={styles.closeTextContainer}>
+            <TouchableOpacity
+              activeOpacity={0.75}
+              onPress={this.onCloseModal}
+            >
+              <Text style={styles.closeText}>SAVE</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  modalContent: {
+    width: '100%',
+    backgroundColor: $white,
+    borderRadius: 3,
+    padding: 20,
+  },
+  editSetTitle: {
+    fontSize: 12,
+    letterSpacing: 2,
+    color: $lightBlue,
+    fontWeight: '800',
+    marginBottom: 20,
+  },
+  modalInput: {
+    height: 40,
+    fontSize: 16,
+    color: $lightBlue,
+    borderBottomColor: $lightBlue,
+    borderBottomWidth: 2,
+  },
+  closeTextContainer: {
+    width: '100%',
+    alignItems: 'flex-end',
+    marginTop: 20,
+  },
+  closeText: {
+    fontSize: 14,
+    fontWeight: '600',
+    color: $darkGreen,
+  },
+});
+
+export default TitleModalEditor;
--- screens/NewSet/NewSetScreen.js
+++ screens/NewSet/NewSetScreen.js
@@ -1,19 +1,53 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 import {
   View, ScrollView, Text, StyleSheet,
 } from 'react-native';
-import { $white } from '../../utils/colors';
+import { $white, $darkBlue } from '../../utils/colors';
 import commonNavigationOptions from '../commonNavigationOptions';
+import RightMenu from '../../components/NewSet/RightMenu';
+import TitleModalEditor from '../../components/NewSet/TitleModalEditor';
 
 class NewSetScreen extends PureComponent {
-  static navigationOptions = {
-    ...commonNavigationOptions,
-    title: 'New Set',
+  static propTypes = {
+    navigation: PropTypes.object.isRequired,
   }
 
+  static navigationOptions = ({ navigation }) => ({
+    ...commonNavigationOptions,
+    title: navigation.getParam('title', 'New Set'),
+    headerRight: <RightMenu navigation={navigation} />,
+    headerStyle: {
+      ...commonNavigationOptions.headerStyle,
+      backgroundColor: navigation.getParam('bgColor', $darkBlue),
+    },
+  })
+
+  state = {
+    modalVisible: false,
+  }
+
+  componentDidMount() {
+    const { navigation } = this.props;
+    navigation.setParams({
+      title: 'New Set',
+      toggleModalVisible: this.toggleModalVisible,
+    });
+  }
+
+  toggleModalVisible = () => this.setState(prev => ({ modalVisible: !prev.modalVisible }))
+
   render() {
+    const { modalVisible } = this.state;
+    const { navigation } = this.props;
+
     return (
       <View style={styles.container}>
+        <TitleModalEditor
+          navigation={navigation}
+          toggleModalVisible={this.toggleModalVisible}
+          visible={modalVisible}
+        />
         <ScrollView style={styles.contentContainer}>
           <Text>NEW SET</Text>
         </ScrollView>
@@ -25,9 +59,7 @@ class NewSetScreen extends PureComponent {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  contentContainer: {
-    flex: 1,
+    padding: 20,
     backgroundColor: $white,
   },
 });
