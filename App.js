7634532e109de6321467de520b9a733c3f8e52ba
rafaelmaiach <rafael.chieregatto@hotmail.com>
2018-12-17 15:23:32 -0200

    feat: Add redux-persist to store

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './store';
+import { store, persistor } from './store';
 import { $darkBlue } from './utils/colors';
 import StatusBar from './components/StatusBar';
 import MainNavigation from './routes/MainNavigation';
 
 const App = () => (
   <Provider store={store}>
-    <View style={styles.container}>
-      <StatusBar backgroundColor={$darkBlue} barStyle="light-content" />
-      <MainNavigation />
-    </View>
+    <PersistGate loading={null} persistor={persistor}>
+      <View style={styles.container}>
+        <StatusBar backgroundColor={$darkBlue} barStyle="light-content" />
+        <MainNavigation />
+      </View>
+    </PersistGate>
   </Provider>
 );
 
