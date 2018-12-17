bd6c338717dedfc6777fcb3eb3e6ffc185217b24
rafael.maia1 <rafael.maia1@ibm.com>
2018-12-17 15:42:47 -0200

    fix: fix time converter function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,12 @@
 const timeConverter = (timestamp) => {
-  const tempDate = new Date(timestamp * 1000);
+  const tempDate = new Date(timestamp);
   const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const year = tempDate.getFullYear();
   const month = months[tempDate.getMonth()];
   const date = tempDate.getDate();
   const hour = tempDate.getHours();
   const min = tempDate.getMinutes();
-  const sec = tempDate.getSeconds();
-  const time = `${date} ${month} ${year} ${hour}:${min}:${sec}`;
+  const time = `${date} ${month} ${year} ${hour}:${min}`;
   return time;
 };
 
